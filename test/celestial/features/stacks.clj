db8f88e29cfe0efa3266123ad4a6c097d0930fa3
ronen <narkisr@gmail.com>
2015-08-10 15:59:38 +0300

    initial stack workflows testing

--- test/celestial/features/stacks.clj
+++ test/celestial/features/stacks.clj
@@ -1,6 +1,7 @@
 (ns celestial.features.stacks
   "Stack feature"
   (:require 
+    [celestial.workflows.stacks :as wf]
     [flatland.useful.map :refer  (dissoc-in*)]
     [celestial.persistency.stacks :as s]
     [celestial.fixtures.data :refer (simple-stack)]
@@ -13,12 +14,15 @@
 
 (with-conf
   (with-state-changes [(before :facts (re-initlize))]
-    (fact "stack persistency" :redis :stacks :persistency 
+    (fact "stack persistency" :integration :stacks :redis :persistency
       (s/add-stack simple-stack) => 1
       (s/update-stack "1" (assoc-in simple-stack [:shared :env] :qa)) 
       (get-in (s/get-stack 1) [:shared :env])  => :qa
-      (s/delete-stack "1") => 1
-      )))
+      (s/delete-stack "1") => 1)
+    
+    (fact "stack staging" :integration :stacks :openstack
+       (s/add-stack simple-stack) => 1
+       (wf/create simple-stack) => nil)))
 
 (fact "stack validations" :validations :stacks
    (s/validate-stack (assoc-in simple-stack [:defaults :dev :machine] [])) => 
